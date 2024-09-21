d6a7392937b2a1c2d17f57d69e6852a607172c93
ninnroot <averyformalname@gmail.com>
2024-09-21 21:43:50 +0800

    fixed stuff

--- django_starter/urls.py
+++ django_starter/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for simbolo_mta_be project.
+URL configuration for django_start project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
